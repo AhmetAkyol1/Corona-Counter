2e0510c54cb9fed7b246383ff964e27cc3863389
halil-kaya <hibrahimkayahik@gmail.com>
2020-03-19 16:29:19 +0300

    first Version

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.InputStream
@@ -21,66 +22,62 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("newone")
+
         getRates()
-
-
-
 
     }
 
+    fun search(view:View){
 
+        var searching=searchText.text.toString()
+
+        searching=searching.toLowerCase()
+        searching=searching.capitalize()
+
+        for(i in 0 until koronas.size){
+            if(koronas.get(i).country.equals(searching)){
+
+                country.text="Country: ${koronas.get(i).country}"
+                cases.text="Cases: ${koronas.get(i).cases}"
+                todayCases.text="Today Cases: ${koronas.get(i).todayCases}"
+                todayDeaths.text="Today Deaths: ${koronas.get(i).todayDeaths}"
+                deaths.text="Deaths: ${koronas.get(i).deaths}"
+                recovered.text="Recovered: ${koronas.get(i).recovered}"
+                active.text="Active: ${koronas.get(i).active}"
+                critical.text="Critical: ${koronas.get(i).critical}"
+                warning.text=""
+                return
+            }
+        }
+        warning.text="Not Found"
+    }
     fun getRates(){
 
-        val downloadData= Download()
-
+        val downloadData = Download()
         try {
-
             val url="https://coronavirus-19-api.herokuapp.com/countries"
             downloadData.execute(url)
 
-
         }catch (e: Exception){
-
         }
-
-
     }
-
-
-
     inner class Download : AsyncTask<String,Void,String>(){
 
         override fun doInBackground(vararg url: String?): String {
 
             var result : String=""
-
-
             var connection = URL(url[0]).openConnection() as HttpsURLConnection
-
             try{
-
                 connection.connect()
                 result=connection.inputStream.use { it.reader().use { reader -> reader.readText() } }
-
-            }catch (e:Exception){
-
             }finally {
                 connection.disconnect()
             }
-
             return result
-
         }
-
-        override fun onPostExecute(result: String?) {
-
+        override fun onPostExecute(result: String?){
             try{
-
-
                 val jsonArray = JSONArray(result)
-
-
                 for (i in 0 until jsonArray.length()){
                     var item = jsonArray.getJSONObject(i)
 
@@ -98,10 +95,6 @@ class MainActivity : AppCompatActivity() {
                     koronas.add(korona)
 
                 }
-
-
-                println(koronas.size)
-
 
 
 
